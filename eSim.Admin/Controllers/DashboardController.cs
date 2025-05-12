1819dd6ebbbee3691a63cb614cb9ced3211f117f
NoOne <muhammadbilaltts41@gmail.com>
2025-05-12 15:35:04 +0500

    bilal-integrated theme

---
+++ eSim.Admin/Controllers/DashboardController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eSim.Admin.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ActionName("Analytics")]
+        public IActionResult Analytics()
+        {
+            return View();
+        }
+
+        [ActionName("CRM")]
+        public IActionResult CRM()
+        {
+            return View();
+        }
+
+        [ActionName("Crypto")]
+        public IActionResult Crypto()
+        {
+            return View();
+        }
+
+        [ActionName("Projects")]
+        public IActionResult Projects()
+        {
+            return View();
+        }
+
+        [ActionName("NFT")]
+        public IActionResult NFT()
+        {
+            return View();
+        }
+
+        [ActionName("Job")]
+        public IActionResult Job()
+        {
+            return View();
+        }
+    }
+}
