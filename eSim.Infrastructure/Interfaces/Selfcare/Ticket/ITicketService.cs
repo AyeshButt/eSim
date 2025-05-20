84f73353adf2ed44797de048a721f6da935d5722
NoOne <muhammadbilaltts41@gmail.com>
2025-05-20 13:11:23 +0500

    bilal-ticket creation and list of ticket

---
+++ eSim.Infrastructure/Interfaces/Selfcare/Ticket/ITicketService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eSim.Infrastructure.DTOs.Global;
+using eSim.Infrastructure.DTOs.Ticket;
+
+namespace eSim.Infrastructure.Interfaces.Selfcare.Ticket
+{
+    public interface ITicketService
+    {
+        public Task<Result<List<TicketsResponseDTO>>> Get();
+
+        public Task<Result<List<TicketTypeResponseDTO>>> GetTicketType();
+        public Task<Result<string>> Create(TicketRequestDTO dto);
+    }
+}
