6104cac7e25565bd77faa98439e4aa4abae25577
AliHassan <alihassan2000@gmail.com>
2025-05-21 21:24:38 +0500

    Task-middleware-subscriber-signupflow-forgotpassword-APi-Message-on-Email

--- eSim.Middleware/Controllers/SubscriberController.cs
+++ eSim.Middleware/Controllers/SubscriberController.cs
@@ -1,7 +1,10 @@
 ﻿using eSim.Implementations.Services.Middleware.Subscriber;
 using eSim.Infrastructure.DTOs.Account;
+using eSim.Infrastructure.DTOs.Email;
 using eSim.Infrastructure.DTOs.Global;
+using eSim.Infrastructure.Interfaces.Admin.Email;
 using eSim.Infrastructure.Interfaces.Middleware;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,9 +12,11 @@ namespace eSim.Middleware.Controllers
 {
     [Route("[controller]")]
     [ApiController]
-    public class SubscriberController(ISubscriberService subscriber) : ControllerBase
+    public class SubscriberController(ISubscriberService subscriber,IEmailService email,IForgotPassword password) : ControllerBase
     {
         private readonly ISubscriberService _subscriber = subscriber;
+        private readonly IEmailService _emailService = email;
+        private readonly IForgotPassword _password = password;
         [HttpGet("check-email")]
         public async Task<IActionResult> CheckEmailExists([FromQuery] string email)
         {
@@ -29,7 +34,7 @@ namespace eSim.Middleware.Controllers
 
             return Ok("Email is available.");
         }
-
+        #region Subscriber
 
         [HttpPost]
 
@@ -40,6 +45,18 @@ namespace eSim.Middleware.Controllers
                 var result = _subscriber.CreateSubscriber(input).GetAwaiter().GetResult();
                 if (result.Success)
                 {
+                    var email = new EmailDTO
+                    {
+                        To = input.Email, 
+                        Body = $"Hi {input.FirstName},\n\nYou are successfully signed up on our platform.\n\nThanks,\neSim Team"
+                    };
+
+                    var emailResult = _emailService.SendEmail(email).GetAwaiter().GetResult();
+                    if (!emailResult.Success)
+                    {
+                        Console.WriteLine("Email error: " + emailResult.Data);
+                     
+                    }
                     return Ok(input);
                 }
                 else
@@ -53,6 +70,48 @@ namespace eSim.Middleware.Controllers
                 return BadRequest();
             }
 
+        }
+        #endregion
+        [AllowAnonymous]
+        [HttpPost("forgot-password")]
+        public async Task<IActionResult> ForgotPassword([FromBody] ForgotPasswordDTO model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var result = await _password.ForgotPasswordAsync(model);
+
+            if (!result.Success)
+                return BadRequest(result.Data);
+
+            return Ok(result.Data);
+        }
+        [AllowAnonymous]
+        [HttpGet("verify-otp")]
+        public async Task<IActionResult> VerifyOtp([FromQuery] string otp)
+        {
+            if (string.IsNullOrEmpty(otp))
+                return BadRequest("OTP is required.");
+
+            var result = await _password.VerifyOtpAsync(otp);
+
+            if (!result.Success)
+                return BadRequest(result.Data);
+
+            return Ok(result.Data);
+        }
+        [HttpPost("reset-password")]
+        public async Task<IActionResult> ResetPassword([FromBody] ResetPasswordDTO model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var result = await _password.ResetPasswordAsync(model);
+
+            if (!result.Success)
+                return BadRequest(result.Data);
+
+            return Ok(result.Data);
         }
     }
 }
