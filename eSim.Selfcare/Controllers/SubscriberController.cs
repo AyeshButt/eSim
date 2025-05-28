dcb66811da18d7fc3e8f324aa711bd92bcc1ac37
NoOne <muhammadbilaltts41@gmail.com>
2025-05-28 13:13:59 +0500

    bilal-committedsubscriber controller

---
+++ eSim.Selfcare/Controllers/SubscriberController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace eSim.Selfcare.Controllers
+{
+    [Authorize]
+    public class SubscriberController : Controller
+    {
+        public IActionResult Subscriber()
+        {
+            return View();
+        }
+    }
+}
