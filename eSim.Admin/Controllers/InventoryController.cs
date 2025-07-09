e608a100fdf3457bacb8528f37a7e5a948af7cea
AyeshButt <ayeshbutt012@gmail.com>
2025-07-09 18:54:27 +0500

    ayesh-task-showing inventory list on admin portal

---
+++ eSim.Admin/Controllers/InventoryController.cs
@@ -0,0 +1,77 @@
+﻿using eSim.Infrastructure.DTOs.Admin.Inventory;
+using eSim.Infrastructure.Interfaces.Admin.Client;
+using eSim.Infrastructure.Interfaces.Admin.Inventory;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace eSim.Admin.Controllers
+{
+    public class InventoryController : Controller
+    {
+        private readonly IInventory _inventory;
+        private readonly IClient _client;
+
+        public InventoryController(IInventory inventory, IClient client)
+        {
+            _inventory = inventory;
+            _client = client;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index(AdminInventoryViewModel input)
+        {
+            var model = new AdminInventoryViewModel();
+            
+            var inventory = await _inventory.GetInventoryAsync();
+            var clients = await _client.GetAllClientsAsync();
+
+            if (inventory is null || clients is null)
+                return View(model);
+
+            model.Inventory = FilterInventory(inventory,input).ToList();
+
+            ViewBag.Clients = clients.Select(u => new SelectListItem() {Text =  u.Name, Value=u.Id.ToString()}).ToList();
+            
+            return View(model);
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> GetClientSubscribers(string clientId)
+        {
+            var subscribers = await _inventory.GetClientSubscribersAsync(clientId);
+
+            if (subscribers is null)
+                return Json(new List<SelectListItem>());
+
+            var model = subscribers.Select(u => new SelectListItem() { Text = $"{u.FirstName} {u.LastName}", Value = u.Id.ToString()}).ToList();
+
+            return Json(model);
+        }
+
+        #region Filter inventory list
+        private IQueryable<AdminInventoryDTO> FilterInventory(IQueryable<AdminInventoryDTO> inventory,AdminInventoryViewModel input)
+        {
+            if (input.Client is not null)
+            {
+                inventory = inventory.Where(u => u.ClientId ==Guid.Parse(input.Client));
+            }
+            if (input.Subscriber is not null)
+            {
+                inventory = inventory.Where(u => u.SubscriberId == Guid.Parse(input.Subscriber));
+            }
+            if (input.From is not null)
+            {
+                inventory = inventory.Where(u => u.CreatedDate >= input.From);
+            }
+            if (input.To is not null)
+            {
+                inventory = inventory.Where(u => u.CreatedDate <= input.To);
+            }
+
+            return inventory;
+        }
+        #endregion
+    }
+}
---
+++ eSim.Implementations/Services/Admin/Inventory/InventoryService.cs
@@ -0,0 +1,72 @@
+﻿using eSim.EF.Context;
+using eSim.Infrastructure.DTOs.Admin.Inventory;
+using eSim.Infrastructure.DTOs.Subscribers;
+using eSim.Infrastructure.Interfaces.Admin.Inventory;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eSim.Implementations.Services.Admin.Inventory
+{
+    public class InventoryService : IInventory
+    {
+        private readonly ApplicationDbContext _db;
+        public InventoryService(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<IQueryable<SubscriberDTO>> GetClientSubscribersAsync(string clientId)
+        {
+            Guid parsedClientId = Guid.Parse(clientId);
+
+            var client_subscriber_list = (from s in _db.Subscribers
+                                          join c in _db.Client on s.ClientId equals c.Id
+                                          where s.ClientId == parsedClientId
+                                          select new SubscriberDTO()
+                                          {
+                                              Id = s.Id,
+                                              FirstName = s.FirstName,
+                                              LastName = s.LastName,
+                                              Email = s.Email,
+                                              Hash = s.Hash,
+                                              Active = s.Active,
+                                              ClientId = s.ClientId,
+                                              CreatedAt = s.CreatedAt,
+                                              ModifiedAt = s.ModifiedAt,
+                                          }
+                                  );
+
+
+            return await Task.FromResult(client_subscriber_list);
+        }
+        public async Task<IQueryable<AdminInventoryDTO>> GetInventoryAsync()
+        {
+            var inventory = (from a in _db.SubscribersInventory
+                             join b in _db.Subscribers on a.SubscriberId equals b.Id
+                             join c in _db.Client on b.ClientId equals c.Id
+                             select new AdminInventoryDTO
+                             {
+                                 Client = c.Name,
+                                 ClientId = c.Id,
+                                 Subscriber = $"{b.FirstName} {b.LastName}",
+                                 Id = a.Id,
+                                 SubscriberId = a.SubscriberId,
+                                 OrderRefrenceId = a.OrderRefrenceId,
+                                 Type = a.Type,
+                                 Item = a.Item,
+                                 Description = a.Description,
+                                 DataAmount = a.DataAmount,
+                                 Duration = a.Duration,
+                                 Country = a.Country,
+                                 Quantity = a.Quantity,
+                                 CreatedDate = a.CreatedDate,
+                                 Assigned = a.Assigned,
+                                 AllowReassign = a.AllowReassign,
+                             });
+            return await Task.FromResult(inventory);
+        }
+    }
+}
