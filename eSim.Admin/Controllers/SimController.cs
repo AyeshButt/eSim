df1d99721a6a948ac770b77511292c7c3f790f2c
NoOne <muhammadbilaltts41@gmail.com>
2025-07-16 20:19:50 +0500

    MB-Admin-Esim-Detail-Filters everything working perfect

--- eSim.Admin/Controllers/SimController.cs
+++ eSim.Admin/Controllers/SimController.cs
@@ -1,28 +1,63 @@
 ﻿using System;
 using System.Threading.Tasks;
+using eSim.Infrastructure.DTOs.Admin.Esim;
+using eSim.Infrastructure.DTOs.Client;
+using eSim.Infrastructure.DTOs.Subscribers;
+using eSim.Infrastructure.Interfaces.Admin.Client;
 using eSim.Infrastructure.Interfaces.Admin.Esim;
 using eSim.Infrastructure.Interfaces.Admin.Order;
+using eSim.Infrastructure.Interfaces.Middleware;
 using eSim.Infrastructure.Interfaces.Middleware.Esim;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace eSim.Admin.Controllers
 {
     public class SimController : Controller
     {
         private readonly IEsims _esim;
+        private readonly ISubscriberService _subscriber;
+        private readonly IClient _client;
 
-        public SimController( IEsims esim)
+        public SimController( IEsims esim, ISubscriberService subscriber, IClient client)
         {
             _esim = esim;
+            _subscriber = subscriber;
+            _client = client;
         }
 
         [HttpGet]
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index( EsimViewModel input)
         {
+            EsimViewModel model = new();
+            var clients = await _client.GetAllClientsAsync();
+            var subScribers = await _subscriber.GetClient_SubscribersListAsync();
+
+            if (subScribers is null || clients is null)
+                return View(model);
+
             var esimsList = await _esim.GetEsimListForAllSubscribersAsync();
-            return View(esimsList);
+
+            model.EsimsResponseList = FilterEsims(esimsList, input).ToList();
+
+            ViewBag.Clients = clients.Select(u => new ClientDTO() { Name = u.Name, Id = u.Id }).ToList();
+
+            return View(model);
         }
 
+
+        [HttpGet]
+        public async Task<IActionResult> GetClientSubscribers(string clientId)
+        {
+            var subscribers = await _subscriber.GetClient_SubscribersListByID(clientId);
+
+            if (subscribers is null)
+                return Json(new List<SelectListItem>());
+
+            var model = subscribers.Select(u => new SelectListItem() { Text = u.Email, Value = u.Id.ToString() }).ToList();
+
+            return Json(model);
+        }
 
         [HttpGet]
         public async Task<IActionResult> Detail(string id)
@@ -49,8 +84,49 @@ namespace eSim.Admin.Controllers
                     var esimHistory = await _esim.GetEsimHistoryAsync(iccid);
                     return PartialView("_EsimHistoryPartialView", esimHistory.Data);
             }
-
             
+        }
+
+
+        private IQueryable<EsimsList> FilterEsims(IQueryable<EsimsList> esims, EsimViewModel input)
+        {
+            if (input == null) return esims;
+
+            // Date Range Filter
+            if (!string.IsNullOrWhiteSpace(input.DateRange))
+            {
+                var dates = input.DateRange.Split(new[] { " to " }, StringSplitOptions.RemoveEmptyEntries);
+
+                if (dates.Length == 2 &&
+                    DateTime.TryParse(dates[0], out var fromDate) &&
+                    DateTime.TryParse(dates[1], out var toDate))
+                {
+                    var to = toDate.Date.AddDays(1).AddSeconds(-1);
+                    esims = esims.Where(u => u.AssignedDate >= fromDate && u.AssignedDate <= to);
+                }
+            }
+
+            // Client Filter
+            if (!string.IsNullOrWhiteSpace(input.Client))
+            {
+
+                esims = esims.Where(u => u.Client == input.Client);
+            }
+
+            // Client Filter
+            if (!string.IsNullOrWhiteSpace(input.Iccid))
+            {
+
+                esims = esims.Where(u => u.Iccid == input.Iccid);
+            }
+
+            // Subscriber Filter
+            if (!string.IsNullOrWhiteSpace(input.Subscriber))
+            {
+                esims = esims.Where(u => u.SubscriberId == input.Subscriber);
+            }
+
+            return esims; // Don't forget to return the filtered query
         }
 
     }
--- eSim.Infrastructure/DTOs/Admin/Esim/EsimViewModel.cs
+++ eSim.Infrastructure/DTOs/Admin/Esim/EsimViewModel.cs
@@ -3,19 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using eSim.Infrastructure.DTOs.Subscribers;
 
 namespace eSim.Infrastructure.DTOs.Admin.Esim
 {
     public class EsimViewModel
     {
-        public string Id { get; set; }
+        public string? Client { get; set; }
+        public string? Subscriber { get; set; }
+        public string? DateRange { get; set; }
+        public string? Iccid { get; set; }
+        public List<EsimsList> EsimsResponseList { get; set; } = new List<EsimsList>();
+    }
+
+    public class EsimsList
+    {
+        public string? Id { get; set; }
         public string SubscriberId { get; set; } = null!;
-        public string SubscriberName { get; set; } 
+        public string? SubscriberName { get; set; }
         public string? Iccid { get; set; }
         public string? CustomerRef { get; set; }
         public string? LastAction { get; set; }
         public DateTime? ActionDate { get; set; }
         public bool? Physical { get; set; }
         public DateTime? AssignedDate { get; set; }
+
+        public string? Client { get; set; }
     }
 }
